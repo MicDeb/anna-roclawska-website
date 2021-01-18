87b4d80cbd7fe23957cf87406660f67b69438024
Michał Dębek <michal.debek@silksh.pl>
2021-01-18 22:17:11 +0100

    Add matchMedia fix

--- src/components/useMedia.js
+++ src/components/useMedia.js
@@ -2,7 +2,7 @@ import { useEffect, useState, useCallback } from 'react';
 
 export default function useMedia(queries, values, defaultValue) {
   const match = useCallback(() => (
-    values[queries.findIndex((q) => matchMedia(q).matches)] || defaultValue
+    values[queries.findIndex((q) => window.matchMedia(q).matches)] || defaultValue
   ), [defaultValue, queries, values]);
   const [value, set] = useState(match);
   useEffect(() => {
