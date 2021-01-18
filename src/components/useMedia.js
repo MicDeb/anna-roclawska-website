86ea4be6c034e3f523438dbc69e83eed58e0fdce
Michał Dębek <michal.debek@silksh.pl>
2021-01-18 22:24:21 +0100

    Add build fix

--- src/components/useMedia.js
+++ src/components/useMedia.js
@@ -1,14 +1,18 @@
 import { useEffect, useState, useCallback } from 'react';
 
 export default function useMedia(queries, values, defaultValue) {
-  const match = useCallback(() => (
-    values[queries.findIndex((q) => window.matchMedia(q).matches)] || defaultValue
-  ), [defaultValue, queries, values]);
-  const [value, set] = useState(match);
-  useEffect(() => {
-    const handler = () => set(match);
-    window.addEventListener('resize', handler);
-    // return () => window.removeEventListener(handler);
-  }, [match]);
-  return value;
+  if (typeof window !== 'undefined') {
+    const match = useCallback(() => (
+      values[queries.findIndex((q) => window.matchMedia(q).matches)] || defaultValue
+    ), [defaultValue, queries, values]);
+    const [value, set] = useState(match);
+    useEffect(() => {
+      const handler = () => set(match);
+      window.addEventListener('resize', handler);
+      // return () => window.removeEventListener(handler);
+    }, [match]);
+    return value;
+  }
+
+  return null;
 }
